6c5d3b374fef6e967099541401166e108574d819
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-04-30 20:52:33 +0100

    feat(datetemplate): add dateTemplate function

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
+import {concatMap} from "@softwareventures/array";
 import {Date} from "@softwareventures/date";
 
 /** A function that formats a Date or part of a Date as a string. */
 export type DateFormatter = (date: Date) => string;
+
+/** Constructs a function that formats a date using the specified template. */
+export function dateTemplate(
+    texts: TemplateStringsArray,
+    ...formatters: readonly DateFormatter[]
+): DateFormatter {
+    return date => concatMap(texts, (text, i) => [text, formatters[i]?.(date)]).join("");
+}
