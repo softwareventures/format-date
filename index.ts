f453e24f3309533f26660e4ec1ff902f068ab921
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-05-01 10:52:16 +0100

    docs(datetemplate): capitalise type name

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import {Date, toReferenceDays} from "@softwareventures/date";
 /** A function that formats a Date or part of a Date as a string. */
 export type DateFormatter = (date: Date) => string;
 
-/** Constructs a function that formats a date using the specified template.
+/** Constructs a function that formats a Date using the specified template.
  *
  * This function is intended to be used as a template literal tag function.
  *
