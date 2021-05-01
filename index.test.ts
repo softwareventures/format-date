52888b6b618d151a93474dbfc7a88c047f7089d0
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-05-01 10:36:35 +0100

    test(iso8601): add test for iso8601 formatter

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import {dayOfWeek} from "./index";
+import {dayOfWeek, iso8601} from "./index";
 
 test("dayOfWeek", t => {
     t.is(dayOfWeek({year: 2021, month: 3, day: 29}), "Monday");
@@ -15,4 +15,11 @@ test("dayOfWeek", t => {
     t.is(dayOfWeek({year: 2021, month: 4, day: 28}), "Wednesday");
     t.is(dayOfWeek({year: 2021, month: 4, day: 29}), "Thursday");
     t.is(dayOfWeek({year: 2021, month: 4, day: 30}), "Friday");
+});
+
+test("iso8601", t => {
+    t.is(iso8601({year: 2021, month: 4, day: 30}), "2021-04-30");
+    t.is(iso8601({year: 10000, month: 1, day: 1}), "10000-01-01");
+    t.is(iso8601({year: 1994, month: 11, day: 5}), "1994-11-05");
+    t.is(iso8601({year: 1, month: 2, day: 3}), "0001-02-03");
 });
